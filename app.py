382dc20d5347629bbe8d58c098d5b6e9ddbc0344
Saifuddin Saifee <saifuddin.saifee@live.co.uk>
2023-08-16 13:56:25 +0530

    Updated App.py -> Basic runable code, adding deps, title, prompt

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import os
 from apikey import apikey
 
+import streamlit as st
+from langchain.llms import OpenAI
+
 os.environ["OPEN_API_KEY"] = apikey
+
+st.title('GPT Creator with Langchain')
+prompt = st.text_input('Plug in your prompt here')
